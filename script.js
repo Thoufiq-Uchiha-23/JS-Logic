e2789b03daa719e0a3050a5ea278960f2af076f7
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-10-09 16:36:52 +0530

    map

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-const names = ["Alice", "Bob", "Charlie", "David", "Emma"]
+// const names = ["Alice", "Bob", "Charlie", "David", "Emma"]
 
 // forEach
-names.forEach(function(value){
-    if(value === "Charlie"){}
-    else {
-        console.log(value)
-    }
-})
+// names.forEach(function(value){
+//     if(value === "Charlie"){}
+//     else {
+//         console.log(value)
+//     }
+// })
+
+// map
+// let newarr = names.map(function(value) {
+//     // return 1;
+//     // return value;
+//     // (5) ['Alice', 'Bob', 'Charlie', 'David', 'Emma']
+//     return value + " ji"
+//     // (5) ['Alice ji', 'Bob ji', 'Charlie ji', 'David ji', 'Emma ji']
+// })
+
+// OUTPUT: 
+// (5) [1, 1, 1, 1, 1]
