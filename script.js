2cff5d10ef696d64e5760550483ca1482f95b4e3
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-10-09 16:06:29 +0530

    for Each

---
+++ script.js
@@ -0,0 +1,9 @@
+const names = ["Alice", "Bob", "Charlie", "David", "Emma"]
+
+// forEach
+names.forEach(function(value){
+    if(value === "Charlie"){}
+    else {
+        console.log(value)
+    }
+})
